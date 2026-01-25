4bb7ddd72ce4a3589862f19f8fd3906b52451e0c
Sudhanshu Singh <sudhanshusingh1402@gmail.com>
2026-01-25 17:40:18 +0530

    I honestly wish I could remember what was going on here...

---
+++ C-Projects/Easy/Number-Guessing-Game/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+    srand(time(0));
+    number = rand() % 100 + 1;
+    
+    printf("Guess a number between 1 and 100\n");
+    
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+        
+        if (guess > number) {
+            printf("Lower!\n");
+        } else if (guess < number) {
+            printf("Higher!\n");
+        } else {
+            printf("You guessed it in %d attempts!\n", attempts);
+        }
+    } while (guess != number);
+    
+    return 0;
+}
